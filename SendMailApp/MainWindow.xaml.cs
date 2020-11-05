6d18c0e534c523f4a78b73b54db0e83472da85c8
SyotaSuzuki <30042@ojs.ac.jp>
2020-11-05 11:19:36 +0900

    設定クラス作成

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -48,14 +48,22 @@ namespace SendMailApp
             try
             {
                 MailMessage msg = new MailMessage("ojsinfosys01@gmail.com", tbTo.Text);
-                msg.Subject = tbTitle.Text;  //件名
-                msg.Body = tbBody.Text;   //本文
+                if (tbCc.Text != "")
+                    msg.CC.Add(tbCc.Text);
 
+                if (tbBcc.Text != "")
+                    msg.Bcc.Add(tbBcc.Text);
+
+                msg.Subject = tbTitle.Text;
+                msg.Body = tbBody.Text;                
+                //msg.Subject = tbTitle.Text;  //件名
+                //msg.Body = tbBody.Text;   //本文
+                
                 sc.Host = "smtp.gmail.com";  //SMTPサーバの設定
                 sc.Port = 587;
                 sc.EnableSsl = true;
                 sc.Credentials = new NetworkCredential("ojsinfosys01@gmail.com", "ojsInfosys2020");
-
+                
                 //sc.Send(msg);   //送信                
                 sc.SendMailAsync(msg);
             }
